25bd66f3f37d042cbdcc5177e89cafbee57eb1bd
LeoLedesma <leofledesma@gmail.com>
2022-03-22 17:44:50 -0300

    Nueva subida

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantidadAMostrar = 4;
+            int numerosMostrados = 0;
+            int numero = 1;
+            int sumaDivisores;
+
+            while (numerosMostrados < cantidadAMostrar)
+            {
+                sumaDivisores = 0;
+                for (int i = 0; i < numero; i++)
+                {
+                    if (i != 0 && numero % i == 0)
+                    {
+                        sumaDivisores += i;
+                    }
+                }
+
+                if (numero == sumaDivisores)
+                {
+                    Console.WriteLine(numero + " es un numero perfecto.");
+                    numerosMostrados++;
+                }
+                numero++;
+            }
+        }
+    }
+}
---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numeroIngresado;
+            long sumaAntes;
+            long sumaDespues;
+
+            Console.Write("Por favor ingrese hasta que numero quiere buscar un centro numerico: ");
+            numeroIngresado = Console.ReadLine();
+
+            if (int.TryParse(numeroIngresado, out int numero))
+            {
+
+                for (int i = 1; i < numero; i++) //i = numeroActual
+                {
+                    sumaAntes = 0;
+                    sumaDespues = 0;
+
+                    for (int j = 0; j < i; j++) //Sumo numeros anteriores
+                    {
+                        sumaAntes += j;
+                    }
+
+                    for (int k = i + 1; sumaDespues <= sumaAntes; k++)//Sumo numeros posteriores y comparo
+                    {
+                        sumaDespues += k;
+                        if (sumaDespues == sumaAntes)
+                        {
+                            Console.WriteLine("{0} es un centro numerico.", i);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("No hemos encontrado mas centros numericos hasta el {0}", numero);
+        }
+    }
+}
