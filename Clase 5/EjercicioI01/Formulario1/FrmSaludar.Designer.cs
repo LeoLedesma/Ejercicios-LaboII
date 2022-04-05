cb0486a8119d1ac0d8cc6851ccd1ae484a725d26
LeoLedesma <leofledesma@gmail.com>
2022-04-04 22:12:08 -0300

    Clase 5 - Ej 1

---
+++ Clase 5/EjercicioI01/Formulario1/FrmSaludar.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Formulario1
+{
+    partial class FrmSaludar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnSaludar = new System.Windows.Forms.Button();
+            this.cmbMateriaFavorita = new System.Windows.Forms.ComboBox();
+            this.lblMateriaFavorita = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNombre.Location = new System.Drawing.Point(37, 33);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(89, 28);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblApellido.Location = new System.Drawing.Point(273, 33);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(91, 28);
+            this.lblApellido.TabIndex = 1;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(37, 64);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(125, 27);
+            this.txtNombre.TabIndex = 0;
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(273, 64);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(125, 27);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // btnSaludar
+            // 
+            this.btnSaludar.Location = new System.Drawing.Point(273, 196);
+            this.btnSaludar.Name = "btnSaludar";
+            this.btnSaludar.Size = new System.Drawing.Size(125, 29);
+            this.btnSaludar.TabIndex = 3;
+            this.btnSaludar.Text = "Saludar";
+            this.btnSaludar.UseVisualStyleBackColor = true;
+            this.btnSaludar.Click += new System.EventHandler(this.btnSaludar_Click);
+            // 
+            // cmbMateriaFavorita
+            // 
+            this.cmbMateriaFavorita.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMateriaFavorita.FormattingEnabled = true;
+            this.cmbMateriaFavorita.Location = new System.Drawing.Point(37, 156);
+            this.cmbMateriaFavorita.Name = "cmbMateriaFavorita";
+            this.cmbMateriaFavorita.Size = new System.Drawing.Size(359, 28);
+            this.cmbMateriaFavorita.TabIndex = 2;
+            // 
+            // lblMateriaFavorita
+            // 
+            this.lblMateriaFavorita.AutoSize = true;
+            this.lblMateriaFavorita.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMateriaFavorita.Location = new System.Drawing.Point(37, 125);
+            this.lblMateriaFavorita.Name = "lblMateriaFavorita";
+            this.lblMateriaFavorita.Size = new System.Drawing.Size(167, 28);
+            this.lblMateriaFavorita.TabIndex = 6;
+            this.lblMateriaFavorita.Text = "Materia favorita";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(37, 97);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(134, 20);
+            this.lblError.TabIndex = 7;
+            this.lblError.Text = "Error solo numeros";
+            this.lblError.Visible = false;
+            // 
+            // FrmSaludar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 237);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.lblMateriaFavorita);
+            this.Controls.Add(this.cmbMateriaFavorita);
+            this.Controls.Add(this.btnSaludar);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmSaludar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "¡Hola, Windows Forms!";
+            this.Load += new System.EventHandler(this.FrmSaludar_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnSaludar;
+        private System.Windows.Forms.ComboBox cmbMateriaFavorita;
+        private System.Windows.Forms.Label lblMateriaFavorita;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ Clase 5/EjercicioI01/Formulario1/FrmSaludar.cs
@@ -0,0 +1,52 @@
+﻿using Formularios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario1
+{
+    public partial class FrmSaludar : Form
+    {
+        public FrmSaludar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludar_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(this.txtApellido.Text) && !string.IsNullOrWhiteSpace(this.txtNombre.Text))
+            {
+                FrmSaludo formSaludo = new FrmSaludo("¡Hola, Windows Forms!", $"Soy {this.txtNombre.Text} {this.txtApellido.Text} y mi materia favorita es {this.cmbMateriaFavorita.Text}.");
+                formSaludo.ShowDialog();
+
+            }else
+            {
+                MessageBox.Show($"Se debe completar los siguientes campos: \n{this.txtNombre.Name}\n{this.txtApellido.Name}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FrmSaludar_Load(object sender, EventArgs e)
+        {
+            this.cmbMateriaFavorita.Items.Add("Programacion I");
+            this.cmbMateriaFavorita.Items.Add("Laboratorio I");
+            this.cmbMateriaFavorita.SelectedIndex = 0; //Indice definido = 0
+            
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.lblError.Visible = false;
+            if (!char.IsLetter(e.KeyChar))
+            {
+                e.Handled = true;
+                this.lblError.Visible = true;
+            }
+        }
+    }
+}
